b583ba5e17517a4c6bfd912f1a04884f844c8835
Gi Wah <giwah_loo@yahoo.es>
2016-12-20 19:54:36 -0500

    se completaron las pruebas para los planes y suscripciones asi como tgambien sus apis. Falta corregir los errores

--- example/test.js
+++ example/test.js
@@ -17,21 +17,19 @@ culqi
   }).then(function (response, body) {
     var token = response.body.id;
     console.log('token', token);
+
     culqi
-      .crearCargo({
+      .crearSuscripcion({
         "token": token,
-        "moneda": "PEN",
-        "monto": 19900,
-        "descripcion": "Venta de prueba",
-        "pedido": shortid.generate(),
         "codigo_pais": "PE",
+        "direccion": "Avenida Lima 123213",
         "ciudad": "Lima",
-        "usuario": "71701956",
-        "direccion": "Avenida Lima 1232",
-        "telefono": 12313123,
-        "nombres": "Will",
-        "apellidos": "Muro",
-        "correo_electronico": "wmuro@me.com"
+        "usuario": "soporteculqi",
+        "telefono": 1234567789,
+        "nombre": "Jon",
+        "apellido": "Doe",
+        "correo_electronico": "soporte@culqi.com",
+        "plan_id": "plan-basico"
       }).then(function (response2) {
         console.log('statusCode2', response2.statusCode);
         console.log('response2.body', response2.body);
